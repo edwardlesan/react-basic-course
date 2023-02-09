70e2fd9a251a2f5f7aae7708cd08016b071c57c9
Eduardo <edwardsendrea1@gmail.com>
2023-02-09 15:15:58 +0200

    react learning

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+
+function App() {
+  const [value, setValue] = useState('Text input')
+
+  return (
+    <div className="App">
+      <ClassCounter/>
+    </div>
+  );
+}
+
+export default App;
